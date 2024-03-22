7a08eb1aa7e11eeb3799a9175e8d144ff40e604d
GunaManivel <gunamani0461@gmail.com>
2024-03-22 13:37:27 +0530

    API home route Added

--- index.js
+++ index.js
@@ -18,6 +18,67 @@ connectDB();
 app.use("/students", StudRouter);
 app.use("/Mentors", MentorRouter);
 
+//Home route
+app.get("/", (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Mentor-Student Management System API</title>
+      <style>
+        body {
+          font-family: Arial, sans-serif;
+          background-color: #f4f4f4;
+          color: #333;
+          margin: 0;
+          padding: 0;
+        }
+        .container {
+          max-width: 800px;
+          margin: 50px auto;
+          padding: 20px;
+          background-color: #fff;
+          border-radius: 8px;
+          box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+        h2 {
+          color: #333;
+          margin-bottom: 20px;
+        }
+        h3 {
+          color: #555;
+          margin-bottom: 10px;
+        }
+        ul {
+          list-style-type: none;
+          padding: 0;
+          text-align: left;
+        }
+        li {
+          margin-bottom: 10px;
+        }
+      </style>
+    </head>
+    <body>
+      <div class="container">
+        <h2>Welcome to the Mentor-Student Management System API!</h2>
+        <h3>API Endpoints :</h3>
+        <ul><br/>
+          <li style="list-style:Numbers;">POST /mentors/create: Create a new mentor.</li><br/>
+          <li style="list-style:Numbers;">POST /students/create: Create a new student.</li><br/>
+          <li style="list-style:Numbers;">POST /students/assign-mentor: Assign a student to a mentor.</li><br/>
+          <li style="list-style:Numbers;">PUT /students/change-mentor/:studentId: Change the mentor for a student.</li><br/>
+          <li style="list-style:Numbers;">GET /mentors/:mentorId/students: Get all students for a particular mentor.</li><br/>
+          <li style="list-style:Numbers;">GET /students/:studentId/previous-mentor: Get the previous mentor for a particular student.</li><br/>
+        </ul>
+      </div>
+    </body>
+    </html>
+  `);
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
